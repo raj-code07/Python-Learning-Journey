1707da356238f1c5bfb6408d4aaa2a3f090844c7
raj-code07 <patidarraj6564@gmail.com>
2025-11-27 00:24:39 +0530

    Add day12 solutions: maximum sum subarray circular

---
+++ day12_maximumSum_Subarray_Circular_BY_Kadane's_Algorithm.py
@@ -0,0 +1,27 @@
+def maxCircularSum(arr):
+    totalSum=0
+    currMaxSum=0
+    currMinSum=0
+    maxSum=arr[0]
+    minSum=arr[0]
+    for i in range(len(arr)):
+        #kadane's maximum sum
+        currMaxSum=max(currMaxSum+arr[i], arr[i])
+        maxSum=max(maxSum, currMaxSum)
+            
+        #kadane's minimum sum
+        currMinSum=min(currMinSum+arr[i], arr[i])
+        minSum=min(minSum, currMinSum)
+            
+        totalSum +=arr[i]
+            
+    normalSum=maxSum
+    circularSum=totalSum-minSum
+        
+    if minSum==totalSum:
+        return normalSum
+        
+    return max(normalSum, circularSum)
+
+arr=[8,-8,9,-9,10,-11,12]
+print(maxCircularSum(arr))

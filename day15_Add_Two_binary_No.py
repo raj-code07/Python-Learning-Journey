60c296ba37f4cbe54efb4d588f8e5c593d1334d7
raj-code07 <patidarraj6564@gmail.com>
2025-11-30 01:50:25 +0530

    Add day 15 solutions : Add two binary numbers

---
+++ day15_Add_Two_binary_No.py
@@ -0,0 +1,38 @@
+def trimleadingZeros(s):
+        firstOne=s.find('1')
+        return s[firstOne:] if firstOne != -1 else "0"
+    
+def addBinary(s1, s2):
+    s1 = trimleadingZeros(s1)
+    s2 = trimleadingZeros(s2)
+    n = len(s1)
+    m = len(s2)
+    if n<m:
+        s1,s2 = s2,s1
+        n,m = m,n
+    j=m-1
+    carry=0
+    result=[]
+        
+    for i in range(n-1,-1,-1):
+        bit1=int(s1[i])
+        bitSum=bit1+carry
+            
+        if j>=0:
+            bit2=int(s2[j])
+            bitSum += bit2
+            j-=1
+                
+        bit = bitSum%2
+        carry=bitSum//2
+            
+        result.append(str(bit))
+        
+    if carry > 0:
+        result.append('1')
+            
+    return ''.join(result[::-1])
+
+s1="1101"
+s2="111"
+print(addBinary(s1,s2))

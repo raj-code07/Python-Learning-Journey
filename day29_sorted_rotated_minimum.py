ce7268065e6aa3639e8cbe24a0a5fb49e95d25e0
raj-code07 <patidarraj6564@gmail.com>
2025-12-14 20:38:45 +0530

    Add day29 solution : sorted rotated minimum

---
+++ day29_sorted_rotated_minimum.py
@@ -0,0 +1,18 @@
+def findMin(arr):
+    lo,hi=0,len(arr)-1
+    while lo<hi:
+        if arr[lo]<arr[hi]:
+            return arr[lo]
+                
+        mid=(lo+hi)//2
+            
+        if arr[mid]>arr[hi]:
+            lo=mid+1
+                
+        else:
+            hi=mid
+    return arr[lo]
+
+
+arr = [5, 6, 1, 2, 3, 4]
+print(findMin(arr))
